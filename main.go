a873ed28d7dbfd780b037002e3e3f725c80fcead
wxsteo <lagosta.robson44@gmail.com>
2023-12-17 11:35:10 +0100

    ho addizionato il loop principale

--- main.go
+++ main.go
@@ -6,9 +6,30 @@ import (
 )
 
 func main() {
-	fmt.Println("Hi I'm chat version 0.0.0")
+	fmt.Println("Ciao sono chat verzione 0.0.0")
 
 	fmt.Println(greetingAccordingTime())
+
+	var command string
+
+	for {
+
+		fmt.Print("Scrivi un commando: ")
+		fmt.Scanln(&command)
+		fmt.Println("Il tuo commando è:", command)
+
+		if command == "uscire" {
+			break
+		}
+
+		if command == "ore" {
+			tempo := time.Now()
+			strtempo := tempo.Format("2006-01-02 15:04")
+			fmt.Println(strtempo)
+		}
+
+		fmt.Println("commando sconosciuto")
+	}
 }
 
 func greetingAccordingTime() string {
@@ -16,14 +37,14 @@ func greetingAccordingTime() string {
 
 	switch {
 	case hour < 12:
-		return "Good Morning!"
+		return "Buongiorno!"
 
 	case hour < 18:
-		return "Good Afternoon!"
+		return "Buon Pomeriggio!"
 
 	case hour < 21:
-		return "Good Evening!"
+		return "Buona Sera!"
 	}
 
-	return "Good Night!"
+	return "Buona Notte!"
 }
